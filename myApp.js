ebac114af21d28a49da3fd80e88f3ea2701edf4d
remchaf <70279203+remchaf@users.noreply.github.com>
2023-05-29 02:49:26 +0000

    Retaking the infosec chapter to achieve ...

--- myApp.js
+++ myApp.js
@@ -1,50 +1,26 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 
+// Max-age const for hsts
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(
+  helmet.hidePoweredBy(),
+  helmet.frameguard({ action: 'deny' }),
+  helmet.xssFilter(),
+  helmet.noSniff(),
+  helmet.ieNoOpen(),
+  helmet.hsts({ maxAge: ninetyDaysInSeconds }),
+  helmet.dnsPrefetchControl(),
+  helmet.noCache(),
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'trusted-cdn.com'],
+    },
+  })
+);
 
 
 module.exports = app;
@@ -52,7 +28,7 @@ const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function (request, response) {
+app.get('/', function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
